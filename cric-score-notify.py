5706d491c736df0bea01f83c663e5daabafd590d
avi <avinashsajjan@gmail.com>
2015-03-26 09:33:47 +0530

    Initial working version, just display code for one time

--- cric-score-notify.py
+++ cric-score-notify.py
@@ -10,21 +10,38 @@ from pync import Notifier
 
 # This script relies on Cricinfo RSS Live Feed (http://static.cricinfo.com/rss/livescores.xml)
 
-cric_info_api = "http://static.cricinfo.com/rss/livescores.xml"
+cric_info_api = 'http://static.cricinfo.com/rss/livescores.xml'
+
+old_score = None
+
+def notify(text):
+	Notifier.notify(text)
 
 while True:
 	score_data_raw = requests.get(cric_info_api)
 	score_data_soup = BeautifulSoup(score_data_raw.text)
 
-	data = score_data_soup.find_all("description")
+	data = score_data_soup.find_all('description')
 
 	if len(data) < 2:
-		print("Looks like no matches are live. Script is exiting!")
+		print('Looks like no matches are live. Script is exiting!')
 		sys.exit()
 
-	print("Following live scores are available now")
+	print('Following live scores are available now')
 	# the first element is always 
 	# <description>Latest scores from Cricinfo</description>
-	for i, match in enumerate(data[1:]): 
-		print(match)
+	for i, game in enumerate(data[1:], 1): 
+		print(i, game.text)
+
+	game_indentifier = int(input('Enter your choice: '))
+	
+	if game_indentifier >= len(data) or game_indentifier < 1:
+		print('Err, wrong choice. I am exiting')
+		sys.exit()
+
+	score = data[game_indentifier].text
+
+	if score != old_score:
+		old_score = score
+		notify(score)
 	break
