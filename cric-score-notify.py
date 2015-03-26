f18dc3a298d7528cbb8c47444554b15fe0e9c336
avi <avinashsajjan@gmail.com>
2015-03-26 10:00:35 +0530

    code cleanup

--- cric-score-notify.py
+++ cric-score-notify.py
@@ -12,8 +12,6 @@ from pync import Notifier
 
 cric_info_api = 'http://static.cricinfo.com/rss/livescores.xml'
 
-old_score = None
-
 def notify(text):
 	Notifier.notify(text)
 
@@ -21,34 +19,37 @@ def notify(text):
 def fetch_score_data():
 	score_data_raw = requests.get(cric_info_api)
 	score_data_soup = BeautifulSoup(score_data_raw.text)
+	score_data = score_data_soup.find_all('description')
 
-	data = score_data_soup.find_all('description')
-
-	if len(data) < 2:
+	if len(score_data) < 2:
 		print('Looks like no matches are live. Script is exiting!')
 		sys.exit()
 
-	return data	
+	return score_data	
 
-data = fetch_score_data()
+def main():
+	old_score = None
+	score_data = fetch_score_data()
+	print('Following live scores are available now')
+	# the first element is always 
+	# <description>Latest scores from Cricinfo</description>
+	for i, game in enumerate(score_data[1:], 1): 
+		print(i, game.text)
 
-print('Following live scores are available now')
-# the first element is always 
-# <description>Latest scores from Cricinfo</description>
-for i, game in enumerate(data[1:], 1): 
-	print(i, game.text)
+	game_indentifier = int(input('Enter your choice: '))
 
-game_indentifier = int(input('Enter your choice: '))
+	if game_indentifier >= len(score_data) or game_indentifier < 1:
+		print('Err, wrong choice. I am exiting')
+		sys.exit()
 
-if game_indentifier >= len(data) or game_indentifier < 1:
-	print('Err, wrong choice. I am exiting')
-	sys.exit()
+	while True:
+		score_data = fetch_score_data()
+		score = score_data[game_indentifier].text
+		if score != old_score:
+			old_score = score
+			notify(score)
+		sleep(5)
+		#break
 
-while True:
-	data = fetch_score_data()
-	score = data[game_indentifier].text
-	if score != old_score:
-		old_score = score
-		notify(score)
-	sleep(5)
-	#break
+if __name__ == '__main__':
+	main()
