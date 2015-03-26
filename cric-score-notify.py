f305a369d678013034738ce5408464e4e95897ca
avi <avinashsajjan@gmail.com>
2015-03-26 14:56:02 +0530

    added proxy support and also command line arguments, so that now user can specify about proxy and fetch frequency

--- cric-score-notify.py
+++ cric-score-notify.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import argparse
 from time import sleep
 
 import requests
@@ -9,15 +10,32 @@ from bs4 import BeautifulSoup
 from pync import Notifier
 
 from settings import cric_info_api
+from settings import cric_info_api_with_proxy
 
 # This script relies on Cricinfo RSS Live Feed (http://static.cricinfo.com/rss/livescores.xml)
+
+parser = argparse.ArgumentParser(description="This script fetches score from Cric Info (using RSS feed) and displays as notification. This script currently works for OS X only.")
+parser.add_argument('-f','--frequency', type=int, default=15, help='This setting specifies how often script should check for score. Provide fetch frequency in seconds.', 
+                    required=False)
+parser.add_argument('-p','--proxy', type=bool, default=False, help='This setting specifies whether to use proxy server to fetch data or not. ', 
+                    required=False)
+options = vars(parser.parse_args())
+
+fetch_frequency = options['frequency']
+use_proxy = options['proxy']
+
 
 def notify(text):
 	Notifier.notify(text)
 
 
 def fetch_score_data():
-	score_data_raw = requests.get(cric_info_api)
+	if use_proxy:
+		url = cric_info_api_with_proxy
+	else:
+		url = cric_info_api
+		
+	score_data_raw = requests.get(url)
 	score_data_soup = BeautifulSoup(score_data_raw.text)
 	score_data = score_data_soup.find_all('description')
 
@@ -26,6 +44,7 @@ def fetch_score_data():
 		sys.exit()
 
 	return score_data	
+
 
 def main():
 	old_score = None
@@ -48,8 +67,9 @@ def main():
 		if score != old_score:
 			old_score = score
 			notify(score)
-		sleep(15)
+		sleep(fetch_frequency)
 		#break
+
 
 if __name__ == '__main__':
 	main()
--- settings.py
+++ settings.py
@@ -1,1 +1,2 @@
 cric_info_api = 'http://static.cricinfo.com/rss/livescores.xml'
+cric_info_api_with_proxy = 'http://anonymouse.org/cgi-bin/anon-www.cgi/http://static.cricinfo.com/rss/livescores.xml'
